29b1b5aee52cc0f804f8729b24738b8cfc86373d
Amir Yankolovich <amiryankolovich2000@gmail.com>
2024-11-25 17:06:30 +0200

    On click the map add marker

--- script.js
+++ script.js
@@ -13,17 +13,27 @@ const inputElevation = document.querySelector('.form__input--elevation');
 
 if (navigator.geolocation)
   navigator.geolocation.getCurrentPosition(function (position) {
-    const { latitude } = position.coords
-    const { longitude } = position.coords
+    const { latitude, longitude } = position.coords
     // console.log(`https://www.google.com/maps/@${latitude},${longitude}`)
     const map = L.map('map').setView([latitude, longitude], 15)
+
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 19,
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map)
+
     L.marker([latitude, longitude]).addTo(map)
-      .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-      .openPopup();
+      .bindPopup('your location.')
+      .openPopup()
+
+    function onMapClick(e) {
+      const { lat, lng } = e.latlng
+      L.marker([lat, lng]).addTo(map)
+        .bindPopup(`lat: ${lat.toFixed(2)}, lng: ${lng.toFixed(2)}`)
+        .openPopup();
+    }
+    map.on('click', onMapClick)
+
   }, function () {
     alert('Could not get your location')
   })
