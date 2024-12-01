a3e0eb742a052faae0f8e1b24b6cbeb879c6eb33
Amir Yankolovich <amiryankolovich2000@gmail.com>
2024-12-01 12:53:00 +0200

    Create Cycling class

--- script.js
+++ script.js
@@ -28,6 +28,12 @@ class Running extends Workout {
     this.cadence = cadence
   }
 }
+class Cycling extends Workout {
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration,)
+    this.elevationGain = elevationGain
+  }
+}
 
 
 class App {
